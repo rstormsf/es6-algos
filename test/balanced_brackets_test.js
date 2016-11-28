0307346c59cd3b0e8474daa66ebf9dcb19070f0d
Roman Storm <rstormsf@gmail.com>
2016-11-28 15:28:48 -0800

    remove describe.only hook

--- test/balanced_brackets_test.js
+++ test/balanced_brackets_test.js
@@ -2,7 +2,7 @@ import {assert} from 'chai';
 import balanced_brackets from '../src/balanced_brackets';
 
 
-describe.only('balanced_brackets', function(){
+describe('balanced_brackets', function(){
   it('returns true if given equal amount of brackets', function(){
     const out = balanced_brackets('[[]]');
     assert.isTrue(out);
